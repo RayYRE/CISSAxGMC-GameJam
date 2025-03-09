4c1d1ac04964da9d777f3f72bc7f3ad90bdec8ed
Aidan H <aidanhalim290@gmail.com>
2025-03-10 00:59:13 +1100

    Added monster

--- scripts/aidan_testmap.gd
+++ scripts/aidan_testmap.gd
@@ -2,11 +2,9 @@ extends Node2D
 
 @onready var tilemap_world1 = $TileMapLayerPresent
 @onready var tilemap_world2 = $TileMapLayerFuture
-@onready var enemies_group = [] 
 var is_world1_active = true
 
 func _ready() -> void:
-	enemies_group = get_tree().get_nodes_in_group("enemies") 
 	tilemap_world1.set_enabled(true)
 	tilemap_world2.set_enabled(false)
 	pass 
@@ -22,13 +20,7 @@ func swap_worlds():
 		tilemap_world1.set_enabled(false)
 		tilemap_world2.set_enabled(true)
 		is_world1_active = false
-		set_enemies_visibility(false)
 	else:
 		tilemap_world1.set_enabled(true)
 		tilemap_world2.set_enabled(false)
 		is_world1_active = true
-		set_enemies_visibility(true)
-	
-func set_enemies_visibility(is_visible: bool) -> void:
-	for enemy in enemies_group:
-		enemy.visible = is_visible
--- scripts/monster.gd
+++ scripts/monster.gd
@@ -5,13 +5,13 @@ extends CharacterBody2D
 @export var detect_range: float = 100.0
 @export var jump_force: float = 200.0
 
-@onready var tilemap_origin: Node = get_tree().current_scene.get_node("TileMapLayerPresent")
+@onready var tilemap_origin: Node = get_tree().current_scene.get_node("TileMapLayerFuture")
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var player: CharacterBody2D = get_tree().current_scene.get_node("Satyr")
 
 func _physics_process(delta: float) -> void:
-	# Disable enemy if tilemap origin is inactive
+	# Disable enemy if origin tilemap is inactive
 	var is_active = tilemap_origin.is_enabled()
 	collision_shape.set_deferred("disabled", not is_active)
 	visible = is_active
@@ -20,6 +20,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 		return
 	
+	animated_sprite.play("walk")
 	# Apply gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta 
@@ -28,18 +29,13 @@ func _physics_process(delta: float) -> void:
 	if global_position.distance_to(player.global_position) < detect_range:
 		var direction = (player.global_position - global_position).normalized()
 		velocity.x = direction.x * speed  
-		animated_sprite.flip_h = direction.x < 0
+		animated_sprite.flip_h = direction.x > 0
 		
 		# Jump if the player is above the enemy
 		if player.global_position.y < global_position.y - 0 and is_on_floor() and is_on_wall():
 			velocity.y = -jump_force
 			
-		# Play walking or idle animation
-		if is_on_floor():
-			animated_sprite.play("walk" if abs(velocity.x) > 1 else "idle")
-			
 	else:
-		animated_sprite.play("idle")
 		velocity.x = 0
 	
 	move_and_slide()
