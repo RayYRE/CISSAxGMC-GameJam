f508ccf190c2190816384ab51898b942f83865c7
RayYRE <rayern@gmail.com>
2025-03-10 16:46:06 +1100

    Added qihan map to game

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-@onready var ORIGINALWORLD = %Original
-@onready var NOHUMANSWORLD = %OtherWorld
+@onready var ORIGINALWORLD = %Lab
+@onready var NOHUMANSWORLD = %Future
 @onready var PLAYER:= %Satyr
 var current_world = ORIGINALWORLD
 var other_world = NOHUMANSWORLD
 var start = 0
 
-
+ 
 func _process(delta: float) -> void:
 	if start == 0:
-		%Original.set_enabled(false)
-		%OtherWorld.set_enabled(true)
+		%Lab.set_enabled(true)
+		%LabBackgrounds.show()
+		%Future.set_enabled(false)
+		%FutureBackground.hide()
 		start = 1
 	
 	if Input.is_action_just_pressed("phaseshift"):
@@ -20,12 +22,20 @@ func _process(delta: float) -> void:
 	
 func swap_worlds():
 	# Store the player's position
-	if current_world == ORIGINALWORLD:
-		world_swap_handler(%Original, %OtherWorld)
+	if %Lab.is_enabled():
+		world_swap_handler(%Lab, %Future)
+		%LabBackgrounds.hide()
+		%FutureBackground.show()
+		%Platform1.set_collision_layer_value(2, false)
 		current_world = NOHUMANSWORLD
 		other_world = ORIGINALWORLD
 	else:
-		world_swap_handler(%OtherWorld, %Original)
+		world_swap_handler(%Future, %Lab)
+		%Platform1.set_collision_layer_value(2, true)
+		%LabBackgrounds.show()
+		%FutureBackground.hide()
+
+		
 		current_world = ORIGINALWORLD
 		other_world = NOHUMANSWORLD
 		
--- scripts/qihantestmap.gd
+++ scripts/qihantestmap.gd
@@ -7,11 +7,11 @@ var current_world = ORIGINALWORLD
 var other_world = NOHUMANSWORLD
 var start = 0
 
-
+ 
 func _process(delta: float) -> void:
 	if start == 0:
 		%Lab.set_enabled(true)
-		%LabBackground.show()
+		%LabBackgrounds.show()
 		%Future.set_enabled(false)
 		%FutureBackground.hide()
 		start = 1
@@ -24,14 +24,17 @@ func swap_worlds():
 	# Store the player's position
 	if %Lab.is_enabled():
 		world_swap_handler(%Lab, %Future)
-		%LabBackground.hide()
+		%LabBackgrounds.hide()
 		%FutureBackground.show()
+		%Platform1.set_collision_layer_value(2, false)
 		current_world = NOHUMANSWORLD
 		other_world = ORIGINALWORLD
 	else:
 		world_swap_handler(%Future, %Lab)
-		%LabBackground.show()
+		%Platform1.set_collision_layer_value(2, true)
+		%LabBackgrounds.show()
 		%FutureBackground.hide()
+
 		
 		current_world = ORIGINALWORLD
 		other_world = NOHUMANSWORLD
